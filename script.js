c86e19f44702d581a1eb37bbc242986a1b78ab1e
NGAM TARYLL <taryllngam@gmail.com>
2023-04-16 10:36:16 +0100

    linters corections to branch

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-const input = document.getElementById('text_input')
-const buttons = document.querySelectorAll('button')
+const input = document.getElementById("text-input");
+const buttons = document.querySelectorAll("button");
 
 let string = "";
-let arr = Array.from(buttons)
+let arr = Array.from(buttons);
 
-arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'AC'){
-            string = ''
-            input.value = string
-        }
-         else if(e.target.innerHTML == 'DEL' ){
-            string = string.substring(0, string.length-1)
-            input.value = string;
-        }
-        else {
-            string += e.target.innerHTML 
-            input .value = string
-        }
-    })
-})
+arr.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
