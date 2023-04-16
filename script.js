865b3ad290094a3d4f5b6186a32f9b2233612cbb
NGAM TARYLL <taryllngam@gmail.com>
2023-04-16 11:19:52 +0100

    linters corections to branch

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const input = document.getElementById('text-input');
-const buttons = document.querySelectorAll('button');
+const input = document.getElementById('text-input')
+const buttons = document.querySelectorAll('button')
 
-let string = '';
-const arr = Array.from(buttons);
+let string = ''
+const arr = Array.from(buttons)
 
 arr.forEach((button) => {
   button.addEventListener('click', (e) => {
-    if (e.target.innerHTML === "=") {
-      string = eval(string);
-      input.value = string;
+    if (e.target.innerHTML === '=') {
+      string = eval(string)
+      input.value = string
     } else if (e.target.innerHTML === 'AC') {
-      string = "";
-      input.value = string;
+      string = ''
+      input.value = string
     } else if (e.target.innerHTML === 'DEL') {
       string = string.substring(0, string.length - 1)
       input.value = string
